09441a9297a54ef550e4a6eb97b38e384c30f710
luc-dev10 <luc.dev1008@gmail.com>
2022-01-27 21:07:42 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   // password
-  password = '';
+  password: string = '';
+  includeLetters: boolean = false;
+  includeNumber: boolean = false;
+  includeSymbol: boolean = false;
+  length: number = 0;
 
-  onButtonClick() {
-    this.password = 'My Password';
+  generatePass() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvxwyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars: String = '';
+
+    // include all letters
+    if (this.includeLetters) validChars += letters;
+
+    // include all numbers
+    if (this.includeNumber) validChars += numbers;
+
+    // symbols
+    if (this.includeSymbol) validChars += symbols;
+
+    // generated pass
+    this.password = '';
+    console.log(this.password);
+
+    for (let i: number = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      this.password += validChars[index];
+    }
+    console.log(this.password);
+  }
+
+  fetchSize(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.length = parseInt(inputElement.value);
+  }
+
+  useLetter() {
+    this.includeLetters = !this.includeLetters;
+  }
+  useNumber() {
+    this.includeNumber = !this.includeNumber;
+  }
+  useSymbol() {
+    this.includeSymbol = !this.includeSymbol;
   }
 }
